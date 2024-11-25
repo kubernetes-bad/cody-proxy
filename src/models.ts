29b2a9afe1f93a93be421a7d0621705d383d580c
kubernetes-bad <titular.jug.0e@icloud.com>
2024-11-25 10:36:13 -0700

    Add Haiku 3.5 v2

--- src/models.ts
+++ src/models.ts
@@ -6,11 +6,12 @@ const modelIds = [
   'anthropic/claude-2.0',  // "chatModelMaxTokens": 12000,
   'anthropic/claude-2.1',
   'anthropic/claude-instant-1.2',  // "completionModelMaxTokens": 9000
-  'anthropic/claude-3-haiku-20240307',  // GATEWAY, "completionModelMaxTokens": 7000
+  'anthropic/claude-3-haiku-20240307', // GATEWAY, "completionModelMaxTokens": 7000
+  'anthropic/claude-3-5-haiku-latest', // GATEWAY, "completionModelMaxTokens": 7000
   'anthropic/claude-3-sonnet-20240229', // GATEWAY, "completionModelMaxTokens": 15000
   'anthropic/claude-3-opus-20240229',  // GATEWAY, "completionModelMaxTokens": 45000
   'anthropic/claude-3-5-sonnet-20240620', // GATEWAY, "completionModelMaxTokens": 45000
-  'anthropic/claude-3-5-sonnet-20241022', // GATEWAY, "completionModelMaxTokens": 45000
+  'anthropic/claude-3-5-sonnet-latest', // GATEWAY, "completionModelMaxTokens": 45000
   'openai/gpt-3.5-turbo',
   'openai/gpt-4-1106-preview',
   'openai/gpt-4-turbo-preview',
@@ -29,11 +30,12 @@ const models: { [key: string]: ModelId } = {
   'Mixtral 8x22B': 'fireworks/accounts/fireworks/models/mixtral-8x22b-instruct',
   'Claude 2.0': 'anthropic/claude-2.0',
   'Claude Instant 1.2': 'anthropic/claude-instant-1.2',
-  'Claude 3 Haiku': 'anthropic/claude-3-haiku-20240307',
+  'Claude 3 Haiku v2': 'anthropic/claude-3-5-haiku-latest',
+  'Claude 3 Haiku v1': 'anthropic/claude-3-haiku-20240307',
   'Claude 3 Sonnet': 'anthropic/claude-3-sonnet-20240229',
   'Claude 3 Opus': 'anthropic/claude-3-opus-20240229',
   'Claude 3.5 Sonnet v1': 'anthropic/claude-3-5-sonnet-20240620',
-  'Claude 3.5 Sonnet v2': 'anthropic/claude-3-5-sonnet-20241022',
+  'Claude 3.5 Sonnet v2': 'anthropic/claude-3-5-sonnet-latest',
   'GPT 3.5 Turbo' : 'openai/gpt-3.5-turbo',
   'GPT 4 Turbo Preview (1106)': 'openai/gpt-4-1106-preview',
   'GPT 4 Turbo Preview': 'openai/gpt-4-turbo-preview',
@@ -72,10 +74,13 @@ const modelQuirks: { [key in ModelId]?: ModelQuirks } = {
   'anthropic/claude-3-haiku-20240307': {
     gateway: true,
   },
+  'anthropic/claude-3-5-haiku-latest': {
+    gateway: true,
+  },
   'anthropic/claude-3-5-sonnet-20240620': {
     gateway: true,
   },
-  'anthropic/claude-3-5-sonnet-20241022': {
+  'anthropic/claude-3-5-sonnet-latest': {
     gateway: true,
   },
   'openai/cody-chat-preview-001': {
